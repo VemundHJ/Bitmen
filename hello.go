12dd18bf74ecc1e352baf125f4f44c1d15c315a8
VemundHJ <35668330+VemundHJ@users.noreply.github.com>
2018-01-26 11:21:17 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
+var leet, hacker, man = "Vemund", "Haugevik", "Jernsletten"
+
 func main() {
 	fmt.Println("Hello, 世界")
   fmt.Println("Knut Henæs")
+  fmt.Println(leet, hacker, man)
 }
